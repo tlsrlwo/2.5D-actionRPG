9b87368760938047a8f1aeba569c7ae8aa0b7a41
tlsrlwo <liongijae@naver.com>
2025-09-23 12:16:24 +0900

    Started testing map system

---
+++ Assets/__KW_Project/Scripts/FrameControlTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KW
+{
+    public class FrameControlTest : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Application.targetFrameRate = 30;
+        }
+    }
+}
