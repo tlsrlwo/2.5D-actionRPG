fdd5fd5d25ae32f9fe9dee90c4d0bc18e2378423
tlsrlwo <liongijae@naver.com>
2025-10-28 20:16:52 +0900

    New Player Sprites

---
+++ Assets/__KW_Project/Editor/ItemDataParsing.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;                  // 1. 에디터 스크립트 필수
+using System.IO;                    // 2. CSV 파일(File) 읽기 필수
+using System.Collections.Generic;   // 3. 딕셔너리(Dictionary) 사용 필수
+using System;                       // 4. Enum, TryParse 등 예외 처리 필수
+
+namespace KW
+{
+    public class ItemDataParsing : MonoBehaviour
+    {
+       
+    }
+}
